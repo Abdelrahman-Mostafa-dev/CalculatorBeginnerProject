4bbed529f65b1fa83dd6fd1b9321389b0d73fe7a
Abdelrahman-Mostafa-dev <54856803+Abdelrahman-Mostafa-dev@users.noreply.github.com>
2020-07-25 13:17:16 +0200

    Add files via upload

---
+++ Calc.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CalculatorBegginerProject
+{
+    class Calc
+    {
+
+        static public void Add()
+        {
+            Console.WriteLine("Please specify the first input");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("Please specify the second input");
+            string secondInput = Console.ReadLine();
+            int num1;
+            int num2;
+            bool IsFirstinputparsable = Int32.TryParse(firstInput, out num1);
+            bool IsSecondinputparsable = Int32.TryParse(secondInput, out num2);
+            if (IsFirstinputparsable && IsSecondinputparsable)
+            {
+                int result = num1 + num2;
+                Console.WriteLine($"The result of {num1} and {num2} is {result}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable && !IsFirstinputparsable)
+            {
+                Console.WriteLine("The inputs are not a number");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsFirstinputparsable)
+            {
+                Console.WriteLine("The first input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable)
+            {
+                Console.WriteLine("The second input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        static public void Subtraction()
+        {
+            Console.WriteLine("Please specify the first input");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("Please specify the second input");
+            string secondInput = Console.ReadLine();
+            int num1;
+            int num2;
+            bool IsFirstinputparsable = Int32.TryParse(firstInput, out num1);
+            bool IsSecondinputparsable = Int32.TryParse(secondInput, out num2);
+            if (IsFirstinputparsable && IsSecondinputparsable)
+            {
+                int result = num1 - num2;
+                Console.WriteLine($"The result of {num1} and {num2} is {result}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable && !IsFirstinputparsable)
+            {
+                Console.WriteLine("The inputs are not a number");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsFirstinputparsable)
+            {
+                Console.WriteLine("The first input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable)
+            {
+                Console.WriteLine("The second input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+
+        }
+        static public void Multiply()
+        {
+            Console.WriteLine("Please specify the first input");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("Please specify the second input");
+            string secondInput = Console.ReadLine();
+            int num1;
+            int num2;
+            bool IsFirstinputparsable = Int32.TryParse(firstInput, out num1);
+            bool IsSecondinputparsable = Int32.TryParse(secondInput, out num2);
+            if (IsFirstinputparsable && IsSecondinputparsable)
+            {
+                int result = num1 * num2;
+                Console.WriteLine($"The result of {num1} and {num2} is {result}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable && !IsFirstinputparsable)
+            {
+                Console.WriteLine("The inputs are not a number");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsFirstinputparsable)
+            {
+                Console.WriteLine("The first input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable)
+            {
+                Console.WriteLine("The second input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        static public void Division()
+        {
+            Console.WriteLine("Please specify the first input");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("Please specify the second input");
+            string secondInput = Console.ReadLine();
+            int num1;
+            int num2;
+            bool IsFirstinputparsable = Int32.TryParse(firstInput, out num1);
+            bool IsSecondinputparsable = Int32.TryParse(secondInput, out num2);
+            if (IsFirstinputparsable && IsSecondinputparsable)
+            {
+                int result = num1 / num2;
+                Console.WriteLine($"The result of {num1} and {num2} is {result}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable && !IsFirstinputparsable)
+            {
+                Console.WriteLine("The inputs are not a number");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsFirstinputparsable)
+            {
+                Console.WriteLine("The first input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable)
+            {
+                Console.WriteLine("The second input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        static public void Exponent()
+        {
+            Console.WriteLine("Please specify the first input");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("Please specify the second input");
+            string secondInput = Console.ReadLine();
+            int num1;
+            int num2;
+            bool IsFirstinputparsable = Int32.TryParse(firstInput, out num1);
+            bool IsSecondinputparsable = Int32.TryParse(secondInput, out num2);
+            if (IsFirstinputparsable && IsSecondinputparsable)
+            {
+                int result = (int)Math.Pow(num1, num2);
+                Console.WriteLine($"The result of {num1} and {num2} is {result}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable && !IsFirstinputparsable)
+            {
+                Console.WriteLine("The inputs are not a number");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsFirstinputparsable)
+            {
+                Console.WriteLine("The first input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsSecondinputparsable)
+            {
+                Console.WriteLine("The second input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        static public void SquareRoot()
+        {
+            Console.WriteLine("Please specify the first input");
+            string firstInput = Console.ReadLine();
+            int num1;
+            bool IsFirstinputparsable = Int32.TryParse(firstInput, out num1);
+            if (IsFirstinputparsable)
+            {
+                int result = (int)Math.Sqrt(num1);
+                Console.WriteLine($"The SquareRoot of {num1} is {result}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (!IsFirstinputparsable)
+            {
+                Console.WriteLine("The first input is not a number!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace CalculatorBegginerProject
+{
+    class Program
+    {   
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Title =  "Calculator";
+                string headText = "Please specify the operation";
+                Console.SetCursorPosition((Console.WindowWidth - headText.Length) / 2, 0);
+                Console.WriteLine(headText);
+                Console.WriteLine("1.Addition \n2.Subtraction\n3.Multiplication\n4.Division\n5.Exponent\n6.SquareRoot");
+                string operation = Console.ReadLine();
+                if (operation == "1")
+                {
+                    Calc.Add();
+                }
+                else if (operation == "2")
+                {
+                    Calc.Subtraction();
+                }
+                else if (operation == "3")
+                {
+                    Calc.Multiply();
+                }
+                else if (operation == "4")
+                {
+                    Calc.Division();
+                }
+                else if (operation == "5")
+                {
+                    Calc.Exponent();
+                }
+                else if (operation == "6")
+                {
+                    Calc.SquareRoot();
+                }
+                else
+                {
+                    Console.WriteLine("Please specify a valid operation");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+
+                
+
+                }
+            }
+        }
+    }
+
